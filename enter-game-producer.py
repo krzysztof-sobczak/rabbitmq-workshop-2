e871f7aff90578343e72df607a43bebe97410ce7
Krzysztof Sobczak <krzysztof@deum.eu>
2017-12-12 00:19:38 +0100

    step 1

---
+++ enter-game-producer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='message-broker'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='enter-game',
+                         exchange_type='fanout')
+
+channel.basic_publish(exchange='enter-game',
+                      routing_key='',
+                      body=message)
+
+print(" [x] Sent enter-game message")
+
+connection.close()
